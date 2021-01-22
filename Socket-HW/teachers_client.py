c3e1791c98a7df56af1765f6fd2f8bf82f246c6d
KokosTech <kokolia1070@gmail.com>
2021-01-22 21:39:08 +0200

    [ADD] Client to teacher's server

---
+++ Socket-HW/teachers_client.py
@@ -0,0 +1,36 @@
+import socket
+import pickle
+
+# ToDo Weather, Time, AirQuality, Non-Existing_Command
+
+
+BUFFER = 1024
+PORT = 8080
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "dis"
+SERVER = "34.77.60.185"  # Change for different server
+ADDR = (SERVER, PORT)
+
+
+def client():
+    cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cl.connect(ADDR)
+    return cl
+
+
+def send(msg, client):
+    d_msg = pickle.dumps(msg)
+    client.send(d_msg)
+
+    data = client.recv(1024)
+    d_data = pickle.loads(pickle.loads(data))
+
+    if msg.lower() == "weather":
+        print(f"The Weather is {d_data[0]['main']} ({d_data[0]['description']})")
+    else:
+        print(d_data)
+
+
+while True:
+    msg = input("Enter a message: ")
+    send(msg, client())
