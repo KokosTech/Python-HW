4db28f1f039a894364ec6ea1ab7047132ddaf5b4
Kaloyan Doychonov <kokolia1070@gmail.com>
2021-05-26 14:18:55 +0300

    [ADD] db

--- Socket-HW/client.py
+++ Socket-HW/client.py
@@ -1,9 +1,17 @@
 import socket
+import sqlite3
 import os
+from sqlite3.dbapi2 import Cursor
+from datetime import datetime
 
 # GUI Libs
 
 import tkinter as tk
+
+# SQLite Connection
+
+con = sqlite3.connect("./db.sqlite3")
+cursor = con.cursor()
 
 # ToDo Weather, Time, AirQuality, Non-Existing_Command
 
@@ -11,12 +19,16 @@ HEADER = 64
 PORT = 50007
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "dis"
-SERVER = "127.0.1.1"  # Change for different server
+SERVER = "192.168.0.148"  # Change for different server
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+def history():
+    query = cursor.execute("SELECT Receive From history_transcript")
+    for i in query:
+        tk.Label(text=i[0]).pack()
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -28,6 +40,9 @@ def send(msg):
     msg = client.recv(2048).decode(FORMAT)
     msgl = tk.Label(text=msg)
     msgl.pack()
+
+    cursor.execute("INSERT INTO history_transcript (Send, Receive, DateTime) VALUES (?, ?, ?)", (message.decode(FORMAT) ,msg, datetime.now().strftime("%H:%M:%S")))
+    con.commit()
 
 def discon():
     send('dis')
@@ -48,6 +63,8 @@ dis = tk.Button(text='Disconnect', command=lambda: discon())
 get_msg.pack()
 button.pack()
 dis.pack()
+
+history()
 
 entry = tk.Entry()
 submit_button = tk.Button()
